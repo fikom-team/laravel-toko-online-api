5b1906590cc78ae1610b79181729a3bd75156768
hananloser <hasyrawi@gmail.com>
2020-08-04 01:09:34 +0800

    adding docs

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,11 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
+/**
+ * Endpoint Get All User Information Has Register
+ *
+ * this is Free Access to See User
+ */
 Route::prefix('auth')
     ->name('auth.')
     ->group(function(){
